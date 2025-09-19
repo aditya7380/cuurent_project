532abf0c23c618b324877f8c53f625d8b32bd648
Aditya Verma <av29032004y@gmail.com>
2025-09-19 19:28:44 +0530

    add the linecs folder

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print("Hello world")
 
 #  git config --global user.email "av29032004y@gmail.com"
-#   git config --global user.name "Aditya Verma"
+#   git config --global user.name "Aditya Verma"ile"
